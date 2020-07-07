20b1b7427121197a79c8993e4828a18a7be78aa8
emmanuel <emmatblingx@gmail.com>
2020-07-07 07:12:16 +0100

    first commit registers state

---
+++ home/js/custom.js
@@ -0,0 +1,40 @@
+$(function () {
+    new WOW().init();
+});
+$(function () {
+    showHideNav();
+
+    $(window).scroll(function () {
+        showHideNav();
+    });
+
+    function showHideNav() {
+
+        if ($(window).scrollTop() > 60) {
+            $("#topper").addClass("hide-top-nav");
+            $("#back-to-top").fadeIn();
+
+        } else {
+
+            $("#topper").removeClass("hide-top-nav");
+            $("#back-to-top").fadeOut();
+        }
+    }
+});
+
+$(function () {
+
+    $("a.smooth-scroll").click(function (event) {
+
+        event.preventDefault();
+
+        // get section id like #about, #servcies, #work, #team and etc.
+        var section_id = $(this).attr("href");
+
+        $("html, body").animate({
+            scrollTop: $(section_id).offset().top - 64
+        }, 1250, "easeInOutExpo");
+
+    });
+
+});
